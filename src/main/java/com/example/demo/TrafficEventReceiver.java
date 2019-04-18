31ed0843ff98cafba289c0cbd6255cd7ddbc71c7
Emmanuel Konzi <maverickkonzinov@gmail.com>
2019-04-18 14:19:30 +0200

    Etape 2 mise en place du stream processing avec KafkaStream

--- src/main/java/com/example/demo/TrafficEventReceiver.java
+++ src/main/java/com/example/demo/TrafficEventReceiver.java
@@ -1,23 +1,27 @@
 package com.example.demo;
 
 import com.example.demo.domain.TrafficEvent;
+import com.example.demo.domain.VehicleClass;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Printed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.stereotype.Component;
 
-
 @Component
-@EnableBinding(Sink.class)
+@EnableBinding(KStreamSink.class)
 public class TrafficEventReceiver {
 
 	private static final Logger logger = LoggerFactory.getLogger(TrafficEvent.class);
 
-	@StreamListener(Sink.INPUT)
-	public void consumeEvent(TrafficEvent event) {
-		logger.info("Received event: {}", event);
+	@StreamListener
+	public void consumeEvent(@Input(KStreamSink.INPUT) KStream<String, TrafficEvent> stream) {
+		logger.info("Received stream: {}", stream);
+		stream.filter((key, event) -> VehicleClass.CAR.equals(event.getVehicleClass()))
+				.print(Printed.toSysOut());
 	}
 
 
